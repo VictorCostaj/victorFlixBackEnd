cd268d7ae45922fd554c5878ef59b3d9a4d87bd9
Victor Costa <victorjosueldacosta@gmail.com>
2023-10-25 01:52:55 -0300

    Atualizando o projeto

---
+++ config/sequelizeCli.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+        dialect: 'postgres',
+        host: 'localhost',
+        port: '5432',
+        database: 'victorflix_development',
+        username: 'victorflix',
+        password: 'victorflix'
+    }
+}
---
+++ src/database/index.ts
@@ -0,0 +1,13 @@
+import {Sequelize } from 'sequelize'
+
+export const database = new Sequelize ({  //instancia 
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'victorflix_development',
+    username: 'victorflix',
+    password: 'victorflix',
+    define: {
+        underscored: true
+    }
+})
