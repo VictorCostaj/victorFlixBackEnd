3bb2bc6ec72f60478012cfc81ef57c9275ee8e97
Victor Costa <victorjosueldacosta@gmail.com>
2023-10-25 03:00:26 -0300

    configurando AdminJS

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,13 +1,36 @@
-import AdminJS from "adminjs";
-import AdminJSExpress from "@adminjs/express";
-import AdminJSSequelize from "@adminjs/sequelize";
-import { Sequelize } from "sequelize";
+// src/adminjs/index.ts
 
-AdminJS.registerAdapter(AdminJSSequelize)
+import AdminJs from 'adminjs'
+import AdminJsExpress from '@adminjs/express'
+import AdminJsSequelize from '@adminjs/sequelize'
+import { database } from '../database'
 
-export const adminjs = new AdminJS ({
-    databases: [Sequelize],
-    rootPath: "/admin",
+AdminJs.registerAdapter(AdminJsSequelize)
+
+export const adminJs = new AdminJs({
+  databases: [database],
+  rootPath: '/admin',
+  branding: {
+    companyName: 'VictorFlix',
+    logo: '/logo-preview.png',
+    theme: {
+      colors: {
+        primary100: '#ff0043',
+	      primary80: '#ff1a57',
+	      primary60: '#ff3369',
+	      primary40: '#ff4d7c',
+		    primary20: '#ff668f',
+	      grey100: '#151515',
+	      grey80: '#333333',
+	      grey60: '#4d4d4d',
+	      grey40: '#666666',
+	      grey20: '#dddddd',
+	      filterBg: '#333333',
+	      accent: '#151515',
+	      hoverBg: '#151515',
+      }
+    }
+  }
 })
 
-export const adminjsRouter = AdminJSExpress.buildRouter(adminjs) // constroi as rotas
+export const adminJsRouter = AdminJsExpress.buildRouter(adminJs)
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import express from 'express'
 import { database } from './database'
+import { adminJs, adminJsRouter } from './admin'
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+app.use(express.static('public'))
 
-app.listen(PORT, () => {
-  database.authenticate().then(() => {
+app.use(adminJs.options.rootPath, adminJsRouter)
+
+const PORT = process.env.port || 3000
+
+app.listen(PORT, async () => {
+  await database.authenticate().then(() => {
     console.log('DB connection successfull.')
   })
-  console.log(`Server started successfuly at port ${PORT}`)
-})
 
+  console.log(`Server started successfuly at port ${PORT}.`)
+})
