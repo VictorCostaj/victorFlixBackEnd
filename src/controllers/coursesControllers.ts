a04ae126d1b21082f3aa3b1349a5ac18f0608f90
Victor Costa <victorjosueldacosta@gmail.com>
2024-04-02 16:48:25 -0300

    Obtendo 3 cursos em destaque

--- src/controllers/coursesControllers.ts
+++ src/controllers/coursesControllers.ts
@@ -1,20 +1,32 @@
 // src/controllers/courseController.ts
 
-import { Request, Response } from 'express'
-import { courseService } from '../services/courseService'
+import { Request, Response } from "express";
+import { courseService } from "../services/courseService";
 
 export const coursesController = {
-  // GET /courses/:id
-  show: async (req: Request, res: Response) => {
-    const { id } = req.params
-
+  // GET /courses/featured
+  featured: async (req: Request, res: Response) => {
     try {
-      const course = await courseService.findByIdWithEpisodes(id)
-      return res.json(course)
+      const featuredCourses = await courseService.getRandomFeaturedCourses();
+      return res.json(featuredCourses);
     } catch (err) {
       if (err instanceof Error) {
-        return res.status(400).json({ message: err.message })
+        return res.status(400).json({ message: err.message });
       }
     }
-  }
-}
+  },
+
+  // GET /courses/:id
+  show: async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const course = await courseService.findByIdWithEpisodes(id);
+      return res.json(course);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+};
