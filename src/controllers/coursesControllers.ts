87206a2669d468f5957973a9fcfde02257361d83
Victor Costa <victorjosueldacosta@gmail.com>
2024-04-02 19:39:05 -0300

    Buscando por cursos

--- src/controllers/coursesControllers.ts
+++ src/controllers/coursesControllers.ts
@@ -1,6 +1,7 @@
 // src/controllers/courseController.ts
 
 import { Request, Response } from "express";
+import { getPaginationParams } from "../helpers/getPaginationParams";
 import { courseService } from "../services/courseService";
 
 export const coursesController = {
@@ -27,6 +28,21 @@ export const coursesController = {
     }
   },
 
+  //GET /courses/search?name=
+  search: async (req: Request, res: Response) => {
+    const { name } = req.query
+    const [page, perPage] = getPaginationParams(req.query)
+    try {
+      if (typeof name !== 'string') throw new Error('name param must be of type string')
+      const courses = await courseService.findByName(name, page, perPage)
+      return res.json(courses)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ messagem: err.message });
+      }
+    }
+  },
+
   // GET /courses/:id
   show: async (req: Request, res: Response) => {
     const { id } = req.params;
@@ -41,3 +57,6 @@ export const coursesController = {
     }
   },
 };
+function async() {
+  throw new Error("Function not implemented.");
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@
 
 import express from "express";
 import { categoriesController } from "./controllers/categoriesController";
-import { coursesController } from "./controllers/coursesControllers";
+import {coursesController}  from "./controllers/coursesControllers";
 
 const router = express.Router();
 
@@ -11,6 +11,7 @@ router.get("/categories/:id", categoriesController.show);
 
 router.get("/courses/featured", coursesController.featured);
 router.get("/courses/newest", coursesController.newest);
+router.get("/courses/search", coursesController.search);
 router.get("/courses/:id", coursesController.show);
 
 export { router };
