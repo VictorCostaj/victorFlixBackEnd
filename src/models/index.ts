391c47270abac47b94c31cc3bd9f400f9609e8d2
Victor Costa <victorjosueldacosta@gmail.com>
2024-04-02 16:15:53 -0300

    Obtendo informações do curso

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,6 +7,8 @@ import { User } from "./User";
 
 Category.hasMany(Course, { as: "courses" });
 
+Course.hasMany(Episode, { as: "episodes" });
+
 Course.belongsTo(Category);
 
 Course.hasMany(Episode);
--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,12 @@
 
 import express from "express";
 import { categoriesController } from "./controllers/categoriesController";
+import { coursesController } from "./controllers/coursesControllers";
 
 const router = express.Router();
 
 router.get("/categories", categoriesController.index);
 router.get("/categories/:id", categoriesController.show);
+router.get("/courses/:id", coursesController.show);
 
 export { router };
